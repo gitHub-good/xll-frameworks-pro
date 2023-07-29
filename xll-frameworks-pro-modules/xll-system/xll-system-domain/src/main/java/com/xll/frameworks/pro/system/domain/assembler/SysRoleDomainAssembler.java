3ffa2cf1433499ab02eb4139ee50fb133199a03e
徐亮亮 <1156033582@qq.com>
2023-07-29 23:41:39 +0800

    RM ID: me feat: 修改版本

--- xll-frameworks-pro-modules/xll-system/xll-system-domain/src/main/java/com/xll/frameworks/pro/system/domain/assembler/SysRoleDomainAssembler.java
+++ xll-frameworks-pro-modules/xll-system/xll-system-domain/src/main/java/com/xll/frameworks/pro/system/domain/assembler/SysRoleDomainAssembler.java
@@ -5,6 +5,15 @@ import com.xll.frameworks.pro.system.repository.po.SysRolePO;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
+/**
+ * 功能描述: <br>
+ * <p>
+ *  <角色model转换>
+ * </p>
+ * @author xuliangliang
+ * @since 2023/7/29 23:38
+ * @version 1.0.0
+ */
 @Mapper
 public interface SysRoleDomainAssembler {
     SysRoleDomainAssembler INSTANCE = Mappers.getMapper(SysRoleDomainAssembler.class );
