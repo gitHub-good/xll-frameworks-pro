93aa8bcdcc39b789e4768c5e0e409949cc2ced97
liangliang.xu <liangliang.xu@ly.com>
2023-08-28 15:44:06 +0800

    feat reactor

--- xll-frameworks-pro-common/xll-common-reactor/src/main/java/com/xll/frameworks/pro/common/reactor/ProReactor.java
+++ xll-frameworks-pro-common/xll-common-reactor/src/main/java/com/xll/frameworks/pro/common/reactor/ProReactor.java
@@ -1,4 +1,16 @@
 package com.xll.frameworks.pro.common.reactor;
 
-public class ProReactor {
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
+public final class ProReactor{
+
+    public static <T> Flux<T> createFlux(T t){
+        return Flux.just(t);
+    }
+
+    public static <T> Flux<T> createFlux(List<T> t){
+        return Flux.fromStream(t.stream());
+    }
 }
